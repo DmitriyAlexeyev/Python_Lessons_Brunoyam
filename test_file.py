13f1123ac79d93d139f5e9b31a4db1234dde58cf
DmitriyAlexeyev <21g@mail.ru>
2022-05-14 18:52:13 +0300

    Pizzeria v2.0 (new branch)

--- test_file.py
+++ test_file.py
@@ -1,4 +1,4 @@
-import json
+# import json
 
 # f = open("test.json", "w")
 # a = json.dump(menu, f)
@@ -6,10 +6,21 @@ import json
 # b = json.loads(a)
 
 
-f = open("test.json", "r")
-a = json.load(f)
-print(a)
-f.close()
+# f = open("test.json", "r")
+# a = json.load(f)
+# print(a)
+# f.close()
+
+# import sys
+# sys.setrecursionlimit(10000)
+#
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     return n * factorial(n - 1)
+#
+#
+# print(factorial(2674))
 
 
 

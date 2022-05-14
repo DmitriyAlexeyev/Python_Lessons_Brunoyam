594dc9afdd3f992da2802f60ae1f7cf54df58d85
DmitriyAlexeyev <21g@mail.ru>
2022-05-14 17:51:26 +0300

    Pizzeria v2.0

--- Pizzeria.py
+++ Pizzeria.py
@@ -1,32 +1,51 @@
 # Администратор (добавляет, удаляет пиццы в меню)
 # Пользователь делает заказ (выбирает пиццы, делает заказ)
+#
+# menu = {
+#     "Margarita": 400,
+#     "Pepperoni": 600
+# }
 
-menu = {
-    "Margarita": 400,
-    "Pepperoni": 600
-}
+import json
+# with open("test.json", "r") as f:
+#     menu = json.load(f)
+
+
+def load_menu():
+    f = open("test.json", "r")
+    menu = json.load(f)
+    f.close()
+    return menu
+
+
+def save_menu(menu):
+    f = open("test.json", "w")
+    json.dump(menu, f)
+    f.close()
 
 
 def add_pizza(name, price):
+    menu = load_menu()
     if name in menu.keys():
         print("Пицца с таким именем уже есть в меню. Придумайте другое название")
     else:
         menu[name] = price
+    save_menu(menu)
 
 
 def remove_pizza(name):
+    menu = load_menu()
     if name in menu.keys():
         print("Пицца с таким именем есть в меню. Пицца удаляется.")
         del menu[name]
     else:
         print("Пиццы с таким именем нет в меню.")
 
-
-list_keys = list(menu.keys())
-list_keys_string = ", ".join(list_keys)
+    save_menu(menu)
 
 
 def order_pizza():
+    menu = load_menu()
     order = []
     cost = 0
     while True:
@@ -46,8 +65,8 @@ def order_pizza():
 if __name__ == "__main__":
     while True:
         q3 = input("Продолжить? (yes/no)")
-        role = input("Выберите роль.")
         if q3 == "yes":
+            role = input("Выберите роль.")
             if role == "admin":
                 q2 = input("Добавить или удалить? (add/remove)")
                 if q2 == "add":
--- test_file.py
+++ test_file.py
@@ -1,13 +1,15 @@
 import json
-from Pizzeria import menu
 
-f = open("test.json", "w")
-a = json.dump(menu, f)
+# f = open("test.json", "w")
+# a = json.dump(menu, f)
+# f.close()
+# b = json.loads(a)
+
+
+f = open("test.json", "r")
+a = json.load(f)
+print(a)
 f.close()
-b = json.loads(a)
-
-
-
 
 
 

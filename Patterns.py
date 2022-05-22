6219b37b7c06855976c081b3475fd32bcf231098
DmitriyAlekseyev <21g@mail.ru>
2022-05-22 18:54:56 +0300

    Patterns 2.0

--- Patterns.py
+++ Patterns.py
@@ -1,16 +1,18 @@
-def my_decorator(function_to_decorate):
-    def wrapper(x, y, z):
-        a = function_to_decorate(x, y)
-        return a * z
-    return wrapper
-
-
-@my_decorator
-def multiply(x, y):
-    return x * y
-
-
-result = multiply(2, 3, 4)
-
-print(result)
+# декоратор
+#
+# def my_decorator(function_to_decorate):
+#     def wrapper(x, y, z):
+#         a = function_to_decorate(x, y)
+#         return a * z
+#     return wrapper
+#
+#
+# @my_decorator
+# def multiply(x, y):
+#     return x * y
+#
+#
+# result = multiply(2, 3, 4)
+#
+# print(result)
 
--- src/22_05.py
+++ src/22_05.py
@@ -1,24 +1,21 @@
-list_to_sort = [3, 1, 87, 5, 2, 17, 43, 8, 23, 11, 20, 7, 10]
+a = [55, 3, 1, 87, 5, 3346, 2, 17, 694, 43, 234, 8, 571, 23, 45, 11, 0, 124, 20, 24, 462, 7, 10]
 
-# for i in range(len(list_to_sort) - 1):
-#     for j in range(len(list_to_sort) - i - 1):
-#         if list_to_sort[j] > list_to_sort[j + 1]:
-#             list_to_sort[j], list_to_sort[j + 1] = list_to_sort[j + 1], list_to_sort[j]
-# print(list_to_sort)
+for i in range(len(a) - 1):
+    for j in range(len(a) - i - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+
+print(a)
+
+for i in range(len(a) - 1):
+    for j in range(len(a) - i - 1):
+        if a[j] < a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+
+print(a)
 
 
-sorted_list = []
-counter = 0
-for i in range(len(list_to_sort) - 1):
-    minimum = list_to_sort[0]
-    counter += 1
-    for j in range(len(list_to_sort) - 1):
-        if list_to_sort[j] < minimum:
-            minimum = list_to_sort[j]
-        print("Несортированный список: ", list_to_sort,
-              "Сортированный список: ", sorted_list,
-              "Количество итераций: ", counter)
-    smallest = minimum
-    sorted_list.append(minimum)
-    list_to_sort.remove(smallest)
-print(list_to_sort, sorted_list, counter)
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+
+print(factorial(5))
